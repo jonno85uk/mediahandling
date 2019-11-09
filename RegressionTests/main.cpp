2468639fc90b5981c519378b447693d1dd51ecbb
jon noble <jonnobleuk@gmail.com>
2019-11-09 17:23:11 +0000

    test: disabled trace

--- RegressionTests/main.cpp
+++ RegressionTests/main.cpp
@@ -1,7 +1,16 @@
 #include <gtest/gtest.h>
+#include "mediahandling.h"
+
+void logFunc(const std::string&)
+{
+  // Do nothing
+}
 
 int main(int argc, char **argv) 
 {
   ::testing::InitGoogleTest(&argc, argv);
+#ifndef LOGGING  
+  media_handling::assignLoggerCallback(&logFunc);
+#endif
   return RUN_ALL_TESTS();
 }
